99f2978cb5928ef16237d7cc4de677fb66a1604c
Gabriel Ciobotaru <gabriel@matatika.com>
2023-09-05 11:07:37 +0300

    add readme and comments to file

--- cloud_config/enable_cloud.sh
+++ cloud_config/enable_cloud.sh
@@ -1,7 +1,12 @@
+#Concatenate the .cloud files, with the .env files required for docker compose
 cat .env.app.cloud >> ../config/.env.app
 cat .env.catalog.cloud >> ../config/.env.catalog
+
+#run docker compose command
 export userID=$(id -u); export groupID=$(id -g)
 ../docker compose up  -> in a new process/thread 
 wait for everything to be up and running.
-nginx -c nginx.conf
+
+#start nginx
+nginx -c <fullpath>/matatika-ce/cloud_config/nginx.conf
 echo Everything is UP!
