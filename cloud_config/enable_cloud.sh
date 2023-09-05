ce98ddbd27ffc862d8faaaaf74da46d72a5fb7cf
Gabriel Ciobotaru <gabriel@matatika.com>
2023-09-05 10:45:36 +0300

    change variables

--- cloud_config/enable_cloud.sh
+++ cloud_config/enable_cloud.sh
@@ -1,5 +1,6 @@
-copy and merge .env.app.cloud to ../config/.env.app
-copy and merge .env.catalog.cloud to ../config/.env.catalog
+cat .env.app.cloud >> ../config/.env.app
+cat .env.catalog.cloud >> ../config/.env.catalog
+export userID=$(id -u); export groupID=$(id -g)
 ../docker compose up  -> in a new process/thread 
 wait for everything to be up and running.
 nginx -c nginx.conf
